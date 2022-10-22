440b529e9ca391b28dca3a60f80dadc9128ad366
Bianca Macedo Hoffer Madruga <bianca.mhm1@gmail.com>
2022-10-22 15:51:25 -0300

    teste página projetos

--- src/components/Project/imageProject.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex, GridItem, Image, useBreakpointValue } from "@chakra-ui/react";
-import styles from './styles.module.scss';
-
-export function ImageProject() {
-    return (
-        <h1>Imagem</h1>
-  ) ;
-}
-
